99a83fce5b96a1c8d661d7dc0ccccb1b94f1db0c
Vee <46942641+vinayaka16041999@users.noreply.github.com>
2021-06-03 00:20:20 +0530

    _

---
+++ Week-7/note_taking_app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+notes = []
+@app.route('/', methods=["GET","POST"])
+def index():
+    note = request.form.get("texts")
+    if (note!=""):
+        notes.append(note)
+    elem = None
+    if elem in notes:
+        notes.remove(elem)
+    return render_template("home.html", notes=notes)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
